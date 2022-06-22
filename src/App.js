9fde559fe2c01aceeef53e17733ddc8a0e4e8692
bhn0108 <bhn0108@icloud.com>
2022-06-22 19:23:29 +0900

    react hooks with useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [count, setCount] = useState(0)
+
+  const incriment = () => {
+    setCount(count + 1)
+  }
+  const decrement = () => {
+    setCount(count - 1)
+  }
+
+  const incriment2 = () => {
+    setCount(previousCount => previousCount + 2)
+  }
+  const decrement2 = () => {
+    setCount(previousCount => previousCount - 2)
+  }
+
+  const reset = () => {
+    setCount(0)
+  }
+
+  const multiply = () => {
+    setCount(count * 2)
+  }
+
+  const divice3 = () => {
+    setCount(previousCount => {
+      return previousCount % 3 === 0 ? previousCount / 3 : previousCount
+    }) 
+  }
+
   return (
-    <div>
-      This is a template for React App.
-    </div>
+    <>
+      <div>count: {count}</div>
+      <div>
+        <button onClick={incriment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={incriment2}>+2</button>
+        <button onClick={decrement2}>-2</button>
+      </div>
+      <div>
+        <button onClick={reset}>Reset</button>
+      </div>
+      <div>
+        <button onClick={multiply}>×2</button>
+      </div>
+      <div>
+        <button onClick={divice3}>3の倍数の時だけ3で割る</button>
+      </div>
+    </>
   )
 }
 
